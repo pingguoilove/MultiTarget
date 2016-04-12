964aa947402443a9c1d432c7c05bf05cc6692894
Zhang Jun <zhangjun0806@gmail.com>
2016-04-12 13:40:45 +0800

    Initial Commit

---
+++ MultiTarget/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MultiTarget
+//
+//  Created by Jun Zhang on 4/12/16.
+//  Copyright © 2016 Jun Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MultiTarget/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MultiTarget
+//
+//  Created by Jun Zhang on 4/12/16.
+//  Copyright © 2016 Jun Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
